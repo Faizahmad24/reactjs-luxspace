a7bc99b4c7d750a168abe361663910b6135d694e
Ahmad Faiz <ahmadfaiz309@gmail.com>
2023-11-23 13:08:16 +0700

    Update : making section for Homepage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 // import logo from './logo.svg';
 // import './App.css';
 import './assets/css/app.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div>
-        <h1 className='text-lg text-red-500 bg-blue-100'>Hello World !!!, there is something NEW GAESSSS AHUUYYY laah TAHUUUUUUUUUWWWWW BULAAAAAATTTT!!!!</h1>
-      </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+    </Router>
+
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../parts/Header'
+import Hero from '../parts/Hero'
+// import Header from '../parts/HEader'
+
+
+export default function HomePage(props) {
+  return (
+    <>
+        <Header></Header>
+        <Hero></Hero>
+    </>
+  )
+}
