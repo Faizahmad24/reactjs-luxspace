56e370ab1688a49b2fb2dad60fa1464271526674
Ahmad Faiz <ahmadfaiz309@gmail.com>
2023-11-24 12:49:40 +0700

    Upadte : Adding Congratulation.js, NotFound.js and PageErrorMessage.js for Success Page and Not Found page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Details from './pages/Details';
 import Cart from './pages/Cart';
+import Congratulation from './pages/Congratulation';
+import NotFound from './pages/NotFound';
 
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/categories/:idc" element={<Details/>}/>
         <Route path="/cart" element={<Cart/>}/>
+        <Route path="/congratulation" element={<Congratulation/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
 
---
+++ src/pages/Congratulation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../parts/Header'
+
+import Sitemap from '../parts/Sitemap'
+import Footer from '../parts/Footer'
+
+import { Link } from 'react-router-dom'
+
+export default function Congratulation() {
+  return (
+    <>
+      <Header theme="black"></Header>
+
+    <Sitemap/>
+    <Footer/>
+  </>
+  )
+}
