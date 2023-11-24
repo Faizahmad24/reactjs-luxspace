4bda575d365fa6c458864dfbe2d79f9176907051
Ahmad Faiz <ahmadfaiz309@gmail.com>
2023-11-24 13:15:03 +0700

    Update : Adding useScrollAbchor.js for custom hooks

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from '../parts/Header'
-import Hero from '../parts/Hero'
+import Hero from '../parts/HomePage/Hero'
 import JustArrived from '../parts/HomePage/JustArrived'
 import BrowseRoom from '../parts/HomePage/BrowseRoom'
 import Clients from '../parts/Clients'
 import Sitemap from '../parts/Sitemap'
 import Footer from '../parts/Footer'
-// import Header from '../parts/HEader'
 
+// Custom Hooks
+import useScrollAchor from '../helpers/hooks/useScrollAchor'
 
 export default function HomePage() {
+  useScrollAchor();
   return (
     <>
         <Header theme="white" position="absolute"></Header>
